32f4a494c810d35a5c9d21a631f9b0902d22811e
naufalnab <naufalnabila27@gmail.com>
2026-06-15 10:24:57 +0700

    Add x_no_faktur_pajak to invoice list view

--- __manifest__.py
+++ __manifest__.py
@@ -3,9 +3,10 @@
     'version': '1.0',
     'category': 'Accounting',
     'summary': 'Menambahkan field x_no_faktur_pajak pada account.move',
-    'description': 'Modul custom untuk memperbaiki error "Invalid field account.move.x_no_faktur_pajak in condition".',
     'depends': ['account'],
-    'data': [],
+    'data': [
+        'views/account_move_views.xml',
+    ],
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
